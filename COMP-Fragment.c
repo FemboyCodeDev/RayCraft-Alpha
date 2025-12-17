f67f4e71b85a3543e5c3c7db9d8b3571ee6b7838
CharsargCodeDevelopment <168666334+CharsargCodeDevelopment@users.noreply.github.com>
2025-12-17 19:50:42 +0000

    Implemented Bloom

--- COMP-Fragment.c
+++ COMP-Fragment.c
@@ -21,6 +21,27 @@ void main()
     }else{
         hdrColor = vec4(0,0,0,0);
     }
+
+    vec2 tex_offset = 1.0 / textureSize(screenTexture, 0); // size of one texel
+
+    int blurSizeX = 8;
+    int blurSizeY = 8;
+    vec4 total_hdr_bloom = vec4(0,0,0,0.0);
+    for (float x=-blurSizeX; x<=blurSizeX; x++){
+        for(float y=-blurSizeY; y<=blurSizeY;y++){
+        vec4 samplePixel = texture(hdrTexture, TexCoords+vec2(x*tex_offset.x,y*tex_offset.y));
+        samplePixel = samplePixel*10;
+        float hdr_sample_luma = samplePixel.r*0.2126 + samplePixel.g*0.7152 + samplePixel.b*0.072;
+        if (hdr_sample_luma > 9){
+
+            total_hdr_bloom += (samplePixel/hdr_sample_luma);
+        }
+    }
+    }
+    total_hdr_bloom = total_hdr_bloom/(blurSizeX*blurSizeY);
+    //total_hdr_bloom = total_hdr_bloom*0.1;
+
+    worldColor += total_hdr_bloom*0.5;
     vec4 skyColor = texture(skyTexture, TexCoords);
 
 
@@ -29,7 +50,10 @@ void main()
         worldColor = texture(skyTexture, TexCoords);
     }
 
+
     fragColor = WorldAndSkyColor;
+
+    //fragColor = total_hdr_bloom;
 
 
 
