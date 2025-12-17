caa1d03d94b9fb34eb25de95cfaf4b18b63a38fb
CharsargCodeDevelopment <168666334+CharsargCodeDevelopment@users.noreply.github.com>
2025-12-17 19:33:40 +0000

    Got the hdr texture into the composite shader, but had to remove a pre-existing input texture to get it to work

--- COMP-Fragment.c
+++ COMP-Fragment.c
@@ -3,7 +3,7 @@ in vec2 TexCoords;
 out vec4 fragColor;
 uniform sampler2D screenTexture;
 uniform sampler2D hdrTexture;
-uniform sampler2D uiTexture;
+//uniform sampler2D uiTexture;
 uniform sampler2D skyTexture;
 uniform sampler2D pauseTexture;
 
@@ -11,43 +11,25 @@ void main()
 {
     fragColor = (texture(screenTexture, TexCoords));
     if (TexCoords.x > 0.5){
-    fragColor = (texture(uiTexture, TexCoords));
+    //fragColor = (texture(uiTexture, TexCoords));
     //fragColor = vec4(0,1.0,0,1.0);
     }
     //fragColor = vec4(gl_FragCoord.xy*vec2(400,300),0.0,1.0);
     
 
 
-    float UI_alpha =  (texture(uiTexture, TexCoords)).a;
+    //float UI_alpha =  (texture(uiTexture, TexCoords)).a;
 
     vec4 worldColor = texture(screenTexture, TexCoords);
 
-
-
-    /*
-    if (worldColor.r > 1){
-        worldColor.r = 1;
-    }else{
-        worldColor.r = 0;
-    }
-    if (worldColor.g > 1){
-        worldColor.g = 1;
-    }else{
-        worldColor.g = 0;
-    }if (worldColor.b > 1){
-        worldColor.b = 1;
-    }else{
-        worldColor.b = 0;
-    }
-    */
-
     vec4 hdrColor = texture(hdrTexture, TexCoords);
     hdrColor = hdrColor*10;
+    //worldColor = worldColor*10;
 
-    float hdr_luma = worldColor.r*0.2126 + worldColor.g*0.7152 + worldColor.b*0.072;
+    float hdr_luma = hdrColor.r*0.2126 + hdrColor.g*0.7152 + hdrColor.b*0.072;
     //vec4 hdrColor = vec4(0,0,0,0);
     if (hdr_luma > 1){
-        hdrColor = worldColor;
+        //hdrColor = worldColor;
     }else{
         hdrColor = vec4(0,0,0,0);
     }
@@ -65,10 +47,16 @@ void main()
         //worldColor = skyTexture;
     }
     //worldColor = texture(skyTexture, TexCoords);
-    fragColor = (WorldAndSkyColor*(1-UI_alpha))+ (texture(uiTexture, TexCoords));
-    fragColor = (fragColor*(1-texture(pauseTexture, TexCoords).a))+texture(pauseTexture, TexCoords);
-    //fragColor = texture(skyTexture, TexCoords);
+    //fragColor = (WorldAndSkyColor*(1-UI_alpha))+ (texture(uiTexture, TexCoords));
 
+    fragColor = WorldAndSkyColor;
+    //fragColor = (fragColor*(1-texture(pauseTexture, TexCoords).a))+texture(pauseTexture, TexCoords);
+
+
+    //fragColor = texture(skyTexture, TexCoords);
+    //fragColor = texture(skyTexture, TexCoords);
+    //fragColor = vec4(UI_alpha,0,0,1);
+    //fragColor = worldColor;
 
 
 
